ebb131f2245d690a264f254440c54a780e775cc1
Eduaro Rodrigues <edusacramento9@gmail.com>
2021-08-08 19:58:06 -0300

    Updade cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.get('/', (request, response) => {
     return response.json({messagem: "Hello World"})
 })
+
+app.use(cors());
 
 app.listen(3333, () => {
     console.log('servidor funcionando');
